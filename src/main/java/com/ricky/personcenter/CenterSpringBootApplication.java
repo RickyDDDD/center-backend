0804a4f5e3f168e378369f1680830e14b01fbd41
ricky <42239599@qq.com>
2023-12-30 17:36:40 +0800

    用户管理中心

---
+++ src/main/java/com/ricky/personcenter/CenterSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.ricky.personcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenterSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CenterSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ricky/personcenter/CenterSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricky.personcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CenterSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
