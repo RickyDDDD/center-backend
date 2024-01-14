44667dfc69a5061367570b5b697d313296963632
ricky <42239599@qq.com>
2024-01-14 21:49:46 +0800

    添加分布式锁redisson

---
+++ src/main/java/com/ricky/personcenter/config/RedissonConfig.java
@@ -0,0 +1,15 @@
+package com.ricky.personcenter.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * redission 配置
+ *
+ * @author Ricky
+ * @date 2024/01/14
+ */
+@Configuration
+public class RedissionConfig {
+
+    
+}
